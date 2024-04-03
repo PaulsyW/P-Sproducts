3da7c5a04daef71289679bd3606aa79eb10f3679
paulsyW <paulsyw3@gmail.com>
2024-04-03 12:51:49 +0300

    Build dynamic navbar

---
+++ script.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('.navbar')
+fetch('/navbar.html')
+    .then(res => res.text())
+    .then(data => {
+        nav.innerHTML = data
+        const parser = newDOMParser()
+        const doc = parser.parserFromString(data, 'text/html')
+        eval(doc.querySelector('script').textContent)
+
+    })
